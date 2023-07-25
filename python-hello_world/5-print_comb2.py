5d8e08343de649c85f887ed4b2dea9774e37241e
Olufemi Odunlami <femi9naija@gmail.com>
2023-07-25 15:36:00 +0100

    Corrected

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,7 +1,2 @@
-for i in range(0,100):
-    if i < 10:
-        print(f"0{i},", end=" ")
-    elif i == 99:
-        print(f"{i}")
-    else:
-        print(f"{i},", end=" ")
+for i in range(100):
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
