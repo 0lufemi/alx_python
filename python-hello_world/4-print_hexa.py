0c291104766e465e44920d4d18dd8260898997f8
Olufemi Odunlami <femi9naija@gmail.com>
2023-07-25 15:10:00 +0100

    Corrected

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,5 +1,6 @@
 for i in range(99):
-    if i < 16:
-        print("{:d} = 0x{:01X}".format(i, i).lower())
-    else:
-        print("{:d} = 0x{:02x}".format(i, i).lower())
+    # if i < 16:
+    #     print("{:d} = 0x{:01X}".format(i, i).lower())
+    # else:
+    #     print("{:d} = 0x{:02x}".format(i, i).lower())
+    print("{:d} = 0x{:02x}".format(i, i).lower())
