21450e2a4628dcc1508b40ef98a24495d91abbb3
Olufemi Odunlami <femi9naija@gmail.com>
2023-08-09 22:25:14 +0100

    1. Lists of lists = Matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,8 @@
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for item in row:
-            print("{:d}".format(item), end=" ")
+    if not matrix:
         print()
+    else:
+        for row in matrix:
+            for item in row:
+                print("{:d}".format(item), end=" ")
+            print()
