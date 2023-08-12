44fc4d64bb7683d317ed10a0689c613daf6964aa
Olufemi Odunlami <femi9naija@gmail.com>
2023-08-12 23:56:43 +0100

    4. Multiply by using map

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,3 +1,3 @@
 def multiply_list_map(my_list=[], number=0):
-    new_list = list(map(lambda number: number * 4, my_list))
+    new_list = list(map(lambda a: a * number, my_list))
     return new_list
