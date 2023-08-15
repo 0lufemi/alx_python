3ec77ea6c92cd7be747c370cbde8b59a18e2dfcd
Olufemi Odunlami <femi9naija@gmail.com>
2023-08-15 19:05:37 +0100

    0. Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,19 @@
+"""Square Class.
+
+This module contains a class that defines a square.
+
+Usage Example:
+
+    Square = __import__('1-square').Square
+
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+"""
+
 class Square:
     """Defines the blueprint of a Square
-    
+
     Attribute:
         size: Integer indicating the size of the object.
     """
