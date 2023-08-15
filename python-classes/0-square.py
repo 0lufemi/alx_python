019cc1ec154d29394c0ab120cb61ffcc27b11f3b
Olufemi Odunlami <femi9naija@gmail.com>
2023-08-15 19:02:26 +0100

    0. Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,14 @@
 class Square:
+    """Defines the blueprint of a Square
+    Attribute:
+        size: Integer indicating the size of the object.
+    """
     def __init__(self, size):
+        """
+        Object constructor method
+        Initializes Square with size
+
+        Arg:
+            size: Integer indicating the size of the object
+        """
         self.__size = size
