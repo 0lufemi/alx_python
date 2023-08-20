c80b8d5d50ba76d32468248b39470a19afd7b0fa
Olufemi Odunlami <femi9naija@gmail.com>
2023-08-20 14:43:18 +0100

    4. Improve Geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,3 +1,12 @@
+"""
+Class that raises an exception
+"""
+
 class BaseGeometry:
+    """
+    Class that raises an exception
+    Function:
+        area
+    """
     def area(self):
         raise Exception("area() is not implemented")
