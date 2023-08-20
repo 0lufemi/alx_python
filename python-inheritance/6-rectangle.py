27bc2a8eacc295cfdf6c3a311def265b931fcca5
Olufemi Odunlami <femi9naija@gmail.com>
2023-08-20 15:15:43 +0100

    6. Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,7 +1,22 @@
+"""
+Program with class Rectangle that inherits from BaseGeometry (5-base_geometry.py).
+"""
+
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
+    """
+    defines a Rectangle class
+    Private instance attributes:
+        width
+        height
+    """
     def __init__(self, width, height):
+        """Initializes an instance
+        Args:
+            width: width of the Rectangle
+            height: height of the Rectangle"""
+
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
