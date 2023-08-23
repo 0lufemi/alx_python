5a201164e51ac12cf4b66645cb7087f1c0fab7b8
Olufemi Odunlami <femi9naija@gmail.com>
2023-08-23 21:50:46 +0100

    7. Update #0

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -108,3 +108,15 @@ class Rectangle(Base):
             print(" " * self.__x, end="")
             for j in range(self.__width):
                 print("#", end="\n" if j == self.__width - 1 else "")
+
+    def update(self, *args):
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
