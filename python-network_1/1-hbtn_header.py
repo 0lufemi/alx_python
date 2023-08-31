34ce7e1a40381934557bdd143c31664b53dcf47a
Olufemi Odunlami <femi9naija@gmail.com>
2023-09-01 00:16:08 +0100

    1. Response header value #1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,15 +5,22 @@ Python script that takes in a URL, sends a request to the URL and displays the v
 import sys
 import requests
 
-url = sys.argv[1]
+def main():
+    url = sys.argv[1]
+    res = requests.get(url)
 
-res = requests.get(url)
+    try:
+        x_request_id = res.headers['X-Request-Id']
+        if x_request_id:
+            print(x_request_id)
+        else:
+            print("None")
+    except requests.exceptions.ConnectionError as e:
+        print("Connection Error:", e)
+    except requests.exceptions.MaxRetryError as e:
+        print("Max Retry Error:", e)
+    except requests.exceptions.RequestException as e:
+        print("An error occurred:", e)
+
 if __name__ == "__main__":
-    # try:
-    x_request_id = res.headers['X-Request-Id']
-    if x_request_id:
-        print(x_request_id)
-    else:
-        print("None")
-    # except requests.exceptions.ConnectionError:
-    #     print(None)
+    main()
