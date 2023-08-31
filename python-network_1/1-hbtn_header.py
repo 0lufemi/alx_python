bb84ff7cad1245cb584c6b7dcdcefc7c9efe0c64
Olufemi Odunlami <femi9naija@gmail.com>
2023-08-31 22:17:17 +0100

    1. Response header value #1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Python script that takes in a URL, sends a request to the URL and displays the value of X-Request_Id
 """
@@ -8,4 +9,11 @@ url = sys.argv[1]
 
 res = requests.get(url)
 if __name__ == "__main__":
-    print(res.headers['X-Request-Id'])
+    # try:
+    x_request_id = res.headers['X-Request-Id']
+    if x_request_id:
+        print(x_request_id)
+    else:
+        print("None")
+    # except requests.exceptions.ConnectionError:
+    #     print(None)
