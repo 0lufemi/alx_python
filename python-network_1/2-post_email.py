ed2b464ee4971bb2e618925e41d65d8ef9ad19eb
Olufemi Odunlami <femi9naija@gmail.com>
2023-08-31 23:29:45 +0100

    2. POST an email #1

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,30 +1,12 @@
 #!/usr/bin/python3
-# """
-# Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as parameter
-# """
-# import requests
-# import sys
-
-# url = sys.argv[1]
-# email = sys.argv[2]
-
-# payload = {"email": email}
-# res = requests.post(url, data= payload)
-# if __name__ == "__name__":
-#     try:
-#         print(f"Your email is {res.text}")
-#     except requests.exceptions.RequestException as e:
-#         print(f"An error occured: {e}")
-
+"""
+Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as parameter
+"""
 
 import requests
 import sys
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python script_name.py <URL> <email>")
-        return
-
     url = sys.argv[1]
     email = sys.argv[2]
 
@@ -32,7 +14,6 @@ def main():
 
     try:
         response = requests.post(url, data=payload)
-        response_body = response.text
         print(f"Email: {email}")
     except requests.exceptions.RequestException as e:
         print("An error occurred:", e)
