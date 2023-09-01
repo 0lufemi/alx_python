dc23de0cb18bf0879f01513a3db7fd2cb542bd14
Olufemi Odunlami <femi9naija@gmail.com>
2023-09-02 00:10:05 +0100

    1. Response header value #1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -7,20 +7,29 @@ import requests
 
 def main():
     url = sys.argv[1]
-    res = requests.get(url)
 
     try:
+        res = requests.get(url)
         x_request_id = res.headers['X-Request-Id']
+        res.raise_for_status()
+
         if x_request_id:
             print(x_request_id)
         else:
-            print("None")
-    except requests.exceptions.ConnectionError as e:
-        print("Connection Error:", e)
-    except requests.exceptions.MaxRetryError as e:
-        print("Max Retry Error:", e)
+            return None
+    # except requests.exceptions.ConnectionError as e:
+    #     print("Connection Error:", e)
+    #     sys.exit(1)
+    # except requests.exceptions.MaxRetryError as e:
+    #     print("Max Retry Error:", e)
+    #     sys.exit(1)
     except requests.exceptions.RequestException as e:
         print("An error occurred:", e)
+        sys.exit(1)
+
+    # except requests.exceptions.HTTPError as e:
+    #     print(f"Error code: {e.res.status_code}")
+    #     sys.exit(1)
 
 if __name__ == "__main__":
     main()
