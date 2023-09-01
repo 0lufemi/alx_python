e16d399d14ff865b66c5db4dcedfe61a1a8a6a25
Olufemi Odunlami <femi9naija@gmail.com>
2023-09-01 23:45:30 +0100

    3. Error code #1

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -7,37 +7,16 @@ import sys
 import requests
 
 def main():
-    # url = sys.argv[1]
-
-    # try:
-    #     res = requests.get(url)
-    #     res.raise_for_status()
-
-    #     if res.status_code >= 400:
-    #         print(f"Error code: {res.status_code}")
-    #     # else:
-    #     #     print(f"Error code: {res.status_code}")
-    # except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError) as e:
-    #     print(f"An error occurred: {e}")
-
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
 
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
-
-        # If the request is successful, print the response body
+        response.raise_for_status()
         print(response.text)
-    except requests.exceptions.RequestException as e:
-        # Handle any request-related exceptions
-        print(f"An error occurred: {e}")
-        sys.exit(1)
+    # except requests.exceptions.RequestException as e:
+    #     print(f"An error occurred: {e}")
+    #     sys.exit(1)
     except requests.exceptions.HTTPError as e:
-        # Handle HTTP errors (4xx and 5xx status codes)
         print(f"Error code: {e.response.status_code}")
         sys.exit(1)
 
