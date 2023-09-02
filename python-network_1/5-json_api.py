ff5c0aa1f21527e651c8153b8d93ac3ae6ea040f
Olufemi Odunlami <femi9naija@gmail.com>
2023-09-02 22:40:59 +0100

    4. Search API

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,60 +1,27 @@
 #!/usr/bin/python3
 """
-Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter
+Use requests package to make a post request to given URL with argument
+set in variable `q`, defaulting to empty string. If response body is properly
+JSON formatted and not empty, display `id` and `name` as given format.
+Otherwise display error message.
 """
-
-import requests
 import sys
-
-def main():
-    # url = "http://0.0.0.0:5000/search_user"
-
-    if len(sys.argv) > 1:
-        letter = sys.argv[1]
-    else:
-        letter = ""
-
-    # try:
-    #     payload = {"q" : letter}
-    #     r = requests.post(url, data = payload)
-    #     r.raise_for_status()
-
-    #     r_json = r.json()
-    #     id = r_json["id"]
-    #     name = r_json["name"]
-
-    #     if len(r_json) != 0:
-    #         print(f"{id} {name}")
-    #     else:
-    #         print("No result")
-    # except Exception:
-    #     print("Not a valid JSON")
-
-    url = "http://0.0.0.0:5000/search_user"
-    params = {"q": letter}
-
-    try:
-        response = requests.post(url, data=params)
-        response_json = response.json()
-
-        if response.status_code == 200:
-            if response_json:
-                user = response_json[0]
-                user_id = user.get('id')
-                user_name = user.get('name')
-                if user_id is not None and user_name is not None:
-                    print(f"[{user_id}] {user_name}")
-                else:
-                    print("No result")
-            else:
-                print("No result")
-        else:
-            print("No result")
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-    except ValueError:
-        print("Not a valid JSON")
+import requests
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+    else:
+        arg = ""
+    payload = {'q': arg}
+    url = "http://0.0.0.0:5000/search_user"
+    r = requests.post(url, data=payload)
+    try:
+        r.raise_for_status()
+        json = r.json()
+        if len(json) == 0:
+            print("No result")
+        else:
+            print("[{:d}] {}".format(json['id'], json['name']))
+    except Exception:
+        print("Not a valid JSON")
