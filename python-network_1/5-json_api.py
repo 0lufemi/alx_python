b996c5102b6091cb6a32c280feddff6786dac200
Olufemi Odunlami <femi9naija@gmail.com>
2023-09-02 22:26:41 +0100

    4. Search API

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -7,27 +7,53 @@ import requests
 import sys
 
 def main():
-    url = "http://0.0.0.0:5000/search_user"
+    # url = "http://0.0.0.0:5000/search_user"
 
     if len(sys.argv) > 1:
         letter = sys.argv[1]
     else:
         letter = ""
 
+    # try:
+    #     payload = {"q" : letter}
+    #     r = requests.post(url, data = payload)
+    #     r.raise_for_status()
+
+    #     r_json = r.json()
+    #     id = r_json["id"]
+    #     name = r_json["name"]
+
+    #     if len(r_json) != 0:
+    #         print(f"{id} {name}")
+    #     else:
+    #         print("No result")
+    # except Exception:
+    #     print("Not a valid JSON")
+
+    url = "http://0.0.0.0:5000/search_user"
+    params = {"q": letter}
+
     try:
-        payload = {"q" : letter}
-        r = requests.post(url, data = payload)
-        r.raise_for_status()
+        response = requests.post(url, params=params)
+        response_json = response.json()
 
-        r_json = r.json()
-        id = r_json["id"]
-        name = r_json["name"]
-
-        if len(r_json) != 0:
-            print(f"{id} {name}")
+        if response.status_code == 200:
+            if response_json:
+                user = response_json[0]
+                user_id = user.get('id')
+                user_name = user.get('name')
+                if user_id is not None and user_name is not None:
+                    print(f"[{user_id}] {user_name}")
+                else:
+                    print("No result")
+            else:
+                print("No result")
         else:
             print("No result")
-    except Exception:
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+    except ValueError:
         print("Not a valid JSON")
 
 if __name__ == "__main__":
