854815192a02469a1d692f4d65d075ad0f6b9933
Olufemi Odunlami <femi9naija@gmail.com>
2023-09-22 13:41:03 +0100

    4. Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute("SELECT cities.id, cities.name, states.name\
-         FROM cities INNER JOIN STATES ON cities.state_id=states.id\
+         FROM cities INNER JOIN states ON cities.state_id=states.id\
             ORDER BY id ASC")
 
     cities = cursor.fetchall()
