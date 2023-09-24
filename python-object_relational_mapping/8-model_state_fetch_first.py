5e6af2aa1a87954f83e6444ac11aa1ff94e66a50
Olufemi Odunlami <femi9naija@gmail.com>
2023-09-24 13:37:27 +0100

    8. First state

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script that prints the first State object from the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}", pool_pre_ping=True
+        )
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    f = session.query(State).first()
+
+    if f:
+        print(f"{f['id']}: {f['name']}")
+    else:
+        print("Nothing")
+
+    session.close()
