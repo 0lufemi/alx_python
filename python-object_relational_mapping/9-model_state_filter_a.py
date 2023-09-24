603f5c11092439d313fd41a11104b198d4544676
Olufemi Odunlami <femi9naija@gmail.com>
2023-09-24 14:01:37 +0100

    9. Contains `a`

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects that
+contain the letter a from the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}",
+        pool_pre_ping=True
+        )
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    results = session.query(State).all()
+
+    for result in results:
+        if 'a' in [result.__dict__['name']]:
+            print(f"{result.__dict__['id']}: {result.__dict__['name']}")
+
+    session.close()
