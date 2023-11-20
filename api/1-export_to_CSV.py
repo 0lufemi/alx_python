b61ec7a00b6d4cc79ea73f13140a49c7d9ee1ebd
Olufemi Odunlami <femi9naija@gmail.com>
2023-11-20 22:55:40 +0100

    1. Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     todo_res = requests.get(todo_url)
     todo_json = todo_res.json()
 
-    with open(f"{employee_id}.csv", "w") as csvfile:
+    with open(f"{employee_id}.csv", "w", encoding='utf8', newline='') as csvfile:
         f = csv.writer(csvfile)
         f.writerow(["USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"])
         for task in todo_json:
